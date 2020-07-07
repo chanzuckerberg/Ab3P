ff24dc35aca3d0cc32b81b98f5ee2f75e7295641
Daniel Hegeman <daniel.hegeman@chanzuckerberg.com>
2020-07-06 23:02:42 -0700

    Create void C connector for use by external programs

--- wrapper.C
+++ wrapper.C
@@ -4,19 +4,23 @@
 #include <cstdio>
 #include <cstring>
 
-extern "C" void get_abbrevs_cpp(char *text, char *output, int *output_size) {
+extern "C" void get_abbrevs_cpp(char *text, char *output_buffer, size_t *output_buffer_size) {
     Ab3P ab3p;
     vector<AbbrOut> abbrs;
     string abbrevs;
 
     ab3p.get_abbrs(text, abbrs); // call main algorithm
-    for (int i = 0; i < abbrs.size(); ++i) {
+    for (size_t i = 0; i < abbrs.size(); ++i) {
         string line = abbrs[i].sf + "|" + abbrs[i].lf + "\n";
         abbrevs.append(line);
     }
-
-    *output_size = abbrevs.length();
-
-    strcpy(output, abbrevs.c_str()); // Copy result into provided buffer
+    
+    size_t output_size = abbrevs.length();
+    
+    if (output_size > *output_buffer_size) {
+        *output_buffer_size = output_size;
+    } else {
+        strcpy(output_buffer, abbrevs.c_str()); // Copy result into provided buffer
+    }
 }
 
